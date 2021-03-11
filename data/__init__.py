fbb535622b794fc0f5d78f9e35812e897436c657
james <james@example.com>
2021-03-11 21:52:26 +0800

    [Dev] regular commit

--- data/__init__.py
+++ data/__init__.py
@@ -1,3 +1,10 @@
+import numpy as np
+
+def add_noise(data: list):
+    data = np.asarray(data)
+    data += np.random.normal(0, 2, len(data))
+    data = data.tolist()
+
 num_mgs = 3 # number of microgrids
 
 # --- Sets ---
